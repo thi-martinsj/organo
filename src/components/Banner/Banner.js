c87b14b97cfcf4ad14de4bc3ddad0e9575028d3d
Thiago Martins <thiagomartins03@hotmail.com>
2023-04-15 17:49:28 -0300

    Create Banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css"
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="Main banner of the Organo page"/>
+        </header>
+    )
+}
+
+export default Banner;
